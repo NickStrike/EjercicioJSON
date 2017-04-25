6fa14721f527b01feefba4836c94da43ad3c610b
817 <817>
2017-04-25 12:10:26 -0300

    Primer Commiterino

---
+++ Ejercicio JSON/Ejercicios/Ejercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿//puedo bajar cualquier tipo de biblioteca de programacion (conjunto de clases), hay que bajar el .dll. (no tocar el codigo fuente), seleccionar la version compatible con mi programa y vincular el .dll y tildar 
+//para hacer esto, en explorador de soluciones ir a referencias, agregar referencia, ver version arriba y examinar. de ahi llegas solito. 
+using System;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titulo;
+
+            titulo = Console.ReadLine();
+            titulo.ToLower();
+
+            WebRequest req = WebRequest.Create("http://www.omdbapi.com/?t=" + titulo);
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            JObject data = JObject.Parse(sr.ReadToEnd());
+
+            string ano = (string)data["Year"];
+
+            Console.WriteLine(ano);
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Ejercicio JSON/Ejercicios/Ejercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ciudad;
+            ciudad = Console.ReadLine();
+            ciudad.ToLower();
+
+            WebRequest req2 = WebRequest.Create("https://maps.googleapis.com/maps/api/geocode/json?address=" + ciudad);
+
+            WebResponse response = req2.GetResponse();
+
+            Stream stream2 = response.GetResponseStream();
+
+            StreamReader streamReader = new StreamReader(stream2);
+
+            JObject datos = JObject.Parse(streamReader.ReadToEnd());
+
+            string pais = (string)datos["results"][0]["address_components"][2]["long_name"];
+
+            Console.WriteLine(pais);
+            Console.ReadLine();
+
+
+        }
+    }
+}
